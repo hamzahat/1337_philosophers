a046c52f15a56977d4f4715150ab18861d895a11
Hamza Benmohamd <hbenmoha@e3r10p17.1337.ma>
2025-07-27 20:29:05 +0100

    add parsin file

--- philo/SRC/parsing.c
+++ philo/SRC/parsing.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils_1.c                                          :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/26 21:21:13 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/07/27 20:26:46 by hbenmoha         ###   ########.fr       */
+/*   Created: 2025/07/27 20:25:37 by hbenmoha          #+#    #+#             */
+/*   Updated: 2025/07/27 20:25:49 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo_header.h"
 
-void    forks_init(t_table *table)
+int	parse_intput(int ac, char *av[])
 {
-    int i;
-
-    i = 0;
-    table->forks_arr = malloc(sizeof(pthread_mutex_t) * table->philo_nb);
-    while (i < table->philo_nb)
-    {
-        pthread_mutex_init(&table->forks_arr[i], NULL);
-        i++;
-    }
-    
-    
-}
-
-void    philos_init(t_table *table)
-{
-    table->philos_arr = malloc(sizeof(t_philo) * table->philo_nb);
+	//todo: check if number of argument is correct! (ac == 5 || ac == 6). if not print an usage example and return from the programe (exit)
+	
+	return (0);
 }
