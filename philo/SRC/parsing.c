a99e24e09d21c07c3f72164268faf0314468feb6
Hamza Benmohamd <hbenmoha@e3r10p17.1337.ma>
2025-07-28 11:00:25 +0100

    add todo list of parsing

--- philo/SRC/parsing.c
+++ philo/SRC/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 20:25:37 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/07/28 09:18:05 by hbenmoha         ###   ########.fr       */
+/*   Updated: 2025/07/28 10:38:02 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@ int	parse_intput(int ac, char *av[], t_table *table)
 {
 	//todo: check if number of argument is correct! (ac == 5 || ac == 6). if not print an usage example and return from the programe (exit)
 	//todo: check if arguments are just numbers not alpha
-	//todo: check if numbers are <= INT_MAX
+	//todo: skip all white spaces at the begining
+	//todo: skip '+' signe at the begining
+	//todo: if we found '-'signe == error
+	//todo: check if numbers are < INT_MAX
+	//todo: check if number of philosophers == 0
+	//todo: handle if number of philosophers == 1
+	//todo: convert all the argv[] to valid numbers
 	
 	return (0);
 }
