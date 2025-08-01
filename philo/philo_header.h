a235059875103f0a260f7add382e8ee511671b2d
Hamza Benmohamd <hbenmoha@e3r10p13.1337.ma>
2025-08-01 12:27:32 +0100

    finish datat init

--- philo/philo_header.h
+++ philo/philo_header.h
@@ -6,12 +6,12 @@
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 18:35:19 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/07/30 17:23:27 by hbenmoha         ###   ########.fr       */
+/*   Updated: 2025/08/01 10:46:49 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#ifndef PHILO_HEADER_H
+# define PHILO_HEADER_H
 
 //* * * Macros for ft_safe_malloc keys:
 # define ALLOCATE 1
@@ -26,8 +26,11 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <limits.h>
+# include <stdbool.h>
 
 //* * * structures * * *//
+
+typedef struct s_table t_table;
 
 //? ft_saf_malloc struct:
 typedef struct s_mem_node
@@ -38,30 +41,34 @@ typedef struct s_mem_node
 
 typedef struct s_fork
 {
-	pthread_mutex_t	fork;		//?
-	int				fork_id;	//?
+	pthread_mutex_t	fork;		//? (mutex) fork
+	int				fork_id;	//? fork id
 }	t_fork;
 
 typedef struct s_philo
 {
-	t_fork			*left_fork;			//?
-	t_fork			*right_fork;		//?
-	long			last_meal_time;		//?
-	int				philo_id;			//?
-	int				nb_meals;			//?
+	t_fork			*second_fork;			//? ptr to t_fork (mutex) left fork
+	t_fork			*first_fork;		//? ptr to t_fork (mutex) right fork
+	long			last_meal_time;		//? the last meal time
+	int				philo_id;			//? philo id
+	int				meals_counter;		//? meals eaten number
+	bool			meals_full;			//? boolian => if the philo eat all the meals = 1
 	pthread_t		thread_id;			//? philo is thread id
+	t_table			*table;				//? ptr to table contents (I don't know i will need it or now)
 }	t_philo;
 
-typedef struct s_table
+struct s_table
 {
 	t_philo			*philos_arr;	//? ptr to an array of philos ( t_philo struct )
 	t_fork			*forks_arr;		//? ptr to an array of forks ( mutex )
 	int				philo_nb;		//? number of philos ( from argv )
-	int				time_to_die;	//?
-	int				time_to_eat;	//?
-	int				time_to_sleep;	//?
+	int				time_to_die;	//? time to die in ms
+	int				time_to_eat;	//? time to eat in ms
+	int				time_to_sleep;	//? time to sleep in ms
 	int				meals_nb;		//? number of meals every philo should eat!
-}	t_table;
+	long			start_simulation;//? the start time of simulation
+	bool			end_simulation; //? a philo die or all philos full (falg 1/0)
+};
 
 
 //* * * functions prototypes * * *//
@@ -71,16 +78,13 @@ typedef struct s_table
 int		parse_intput(int ac, char *av[], t_table *table);
 void	ft_putstr_fd(int fd, char *str);
 size_t	ft_strlen(char *str);
-int		check_args_number(int ac);
-int		converte_args_to_nb(t_table *table, char *av[]);
 int		ft_atoi(char *str);
 void	printf_input_data(t_table table);
-void	initialize_table_data(t_table *table);
 
 //? --- data initialization ---
 
-void	philos_init(t_table *table);
-void	forks_init(t_table *table);
+int	philos_init(t_table *table);
+int	forks_init(t_table *table);
 
 //? --- garbage collector functions ---
 
