ce8dd05e09177f1612daffca63033d4d481041d3
Hamza Benmohamd <hbenmoha@e3r10p15.1337.ma>
2025-08-14 22:28:24 +0100

    [feature] add check for philo nb to be < 200 and timestamps to be > 60

--- new_philo/philo_main.c
+++ new_philo/philo_main.c
@@ -6,7 +6,7 @@
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 10:38:03 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/08/14 18:40:44 by hbenmoha         ###   ########.fr       */
+/*   Updated: 2025/08/14 22:10:19 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,22 @@ int	main(int ac, char *av[])
 }
 
 
+//todo: add check in parsing of philo_nbr < 200 timestapms > 60
 //todo: handke if something failed!
+
 //? why you do the monitor as a single thread and not make the main thread as monitor
-//todo: fix parsing error => accept just the clean numbers ( 123abc => is not valide )
 //todo: go and check the last part of video of oceano when he talk about time to eat should be time to die * 2 to let philo live
 //todo: read about pthread_detach .. 
 //todo: what are silbiyat dyal treads
 //todo: what is zombi thread
 //todo: remove the debuggin messages! + falgs from makefile
 /*
-todo: why we use pthread_join ( answer => to free the stack of the thread) 
-todo: but we can use wait  to wait for a thread and the stack will not give as a leak,
-todo: because the stack_area of thread was in stack not heap,
-todo: so when the int main exit it free all the vars ( memory ), 
-todo: but the problem happen when the main thread exit befor the other threads finish so it give as a leak,
-todo: so pthread_join want for the thread until it finish,
-todo: so it let the main thread to free anything ( stack ) => no leak 
-todo: (so that's why the pthread_join fun does not free memeory of thead it just want for it and let main to free everyting )
+ why we use pthread_join ( answer => to free the stack of the thread) 
+ but we can use wait  to wait for a thread and the stack will not give as a leak,
+ because the stack_area of thread was in stack not heap,
+ so when the int main exit it free all the vars ( memory ), 
+ but the problem happen when the main thread exit befor the other threads finish so it give as a leak,
+ so pthread_join want for the thread until it finish,
+ so it let the main thread to free anything ( stack ) => no leak 
+ (so that's why the pthread_join fun does not free memeory of thead it just want for it and let main to free everyting )
 */
--- new_philo/philo_parsing.c
+++ new_philo/philo_parsing.c
@@ -6,7 +6,7 @@
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 10:42:18 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/08/12 13:40:40 by hbenmoha         ###   ########.fr       */
+/*   Updated: 2025/08/14 22:27:08 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,28 @@
 
 int	parse_input(int ac, char *av[])
 {
+	int philo_nb;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	meals_nbr;
+
+	philo_nb = ft_atoi(av[1]);
+	time_to_die = ft_atoi(av[2]);
+	time_to_eat = ft_atoi(av[3]);
+	time_to_sleep = ft_atoi(av[4]);
+	meals_nbr = ft_atoi(av[5]);
 	if (ac != 5 && ac != 6)
 		return (ft_putstr_fd(2, "Error: wrong number of arguments\n"), 1);
-	if (ft_atoi(av[1]) < 1)
+	if (philo_nb < 1 || philo_nb > 200)
 		return (ft_putstr_fd(2, "Error: number_of_philosophers is not correct!\n"), 1);
-	if (ft_atoi(av[2]) < 1)
+	if (time_to_die < 1 || time_to_die < 60)
 		return (ft_putstr_fd(2, "Error: time_to_die is not correct!\n"), 1);
-	if (ft_atoi(av[3]) < 1)
+	if (time_to_eat < 1 || time_to_eat < 60)
 		return (ft_putstr_fd(2, "Error: time_to_eat is not correct!\n"), 1);
-	if (ft_atoi(av[4]) < 1)
+	if (time_to_sleep < 1 || time_to_sleep < 60)
 		return (ft_putstr_fd(2, "Error: time_to_sleep is not correct!\n"), 1);
-	if (av[5] && ft_atoi(av[5]) < 1)
+	if (av[5] && meals_nbr < 1)
 		return (ft_putstr_fd(2, "Error: number_of_times_each_philosopher_must_eat is not correct!\n"), 1);
 	return (printf("correct input!\n"), 0); //todo: remove this msg
 }
